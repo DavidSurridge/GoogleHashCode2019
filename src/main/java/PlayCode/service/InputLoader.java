37fe63479c3369f897a5dfb1dcce7de92e981ae8
Allan Neves Haerber <allan_hkrs@hotmail.com>
2019-02-28 13:42:29 +0000

    Refactored InputValues to create an InputLoader Service to parse the input

---
+++ src/main/java/PlayCode/service/InputLoader.java
@@ -0,0 +1,44 @@
+package PlayCode.service;
+
+import PlayCode.model.Payload;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class InputLoader {
+
+    private static final String INPUT_FILENAME = "ExampleInput";
+
+    public InputLoader() {
+        final File file = loadFileFromResources();
+        parseFileContentToPayload(file);
+    }
+
+    private File loadFileFromResources() {
+        final ClassLoader classLoader = getClass().getClassLoader();
+        final String fileName = classLoader.getResource(INPUT_FILENAME).getFile();
+
+        if (fileName == null) {
+            throw new RuntimeException("Could not load the file from the resources folder.");
+        }
+
+        return new File(fileName);
+    }
+
+    private void parseFileContentToPayload(File file) {
+        try {
+            final List<String> content = FileUtils.readLines(file);
+            content.forEach(System.out::println);
+            //int[] Types = Arrays.asList(myData).stream().mapToInt(Integer::parseInt).toArray();
+            //ArrayList<String> identifiers = new ArrayList<String>(List.of(line.split(" ")));
+        } catch (IOException e) {
+            throw new RuntimeException("The file cannot be read due to invalid characters.");
+        }
+    }
+
+    public Payload getPayload() {
+        return new Payload();
+    }
+}
