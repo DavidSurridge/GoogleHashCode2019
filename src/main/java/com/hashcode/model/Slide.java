b75a90784c76283e7a56367699f72b8f9334be1a
David Surridge <davidsurridge@gmail.com>
2019-02-28 19:08:51 +0000

    add slide class

--- src/main/java/com/hashcode/model/Slide.java
+++ src/main/java/com/hashcode/model/Slide.java
@@ -1,11 +1,24 @@
 package com.hashcode.model;
 
+
 import lombok.Data;
 
-import java.util.List;
+import java.util.ArrayList;
+
 
 @Data
 public final class Slide {
 
-    private List<Photo> photos;
+    ArrayList<Photo> allphotos = new ArrayList<Photo>();
+
+    public Slide(Photo photo) {
+        allphotos.add(photo);
+    }
+
+    public Slide(Photo photo1, Photo photo2) {
+        allphotos.add(photo1);
+        allphotos.add(photo2);
+    }
+
+
 }
