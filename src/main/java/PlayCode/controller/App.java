af15f1a91f231a65378b939646246733ee246846
Allan Neves Haerber <allan_hkrs@hotmail.com>
2019-02-28 10:29:31 +0000

    Tidying up

--- src/main/java/PlayCode/controller/App.java
+++ src/main/java/PlayCode/controller/App.java
@@ -1,23 +1,19 @@
 package PlayCode.controller;
 
-import java.io.IOException;
-
 import PlayCode.constants.InputValues;
+
+import java.io.IOException;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-      
-      try {
-        InputValues values = new InputValues("src/main/java/PlayCode/constants/ExampleInput");
-      } catch (IOException e) {
-       
-        e.printStackTrace();
-      }
+public class App {
+
+    public static void main(String[] args) {
+        try {
+            InputValues values = new InputValues("src/main/java/PlayCode/constants/ExampleInput");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/PlayCode/export/ExportSolution.java
+++ src/main/java/PlayCode/export/ExportSolution.java
@@ -6,27 +6,23 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
-public class ExportSolution {
+public final class ExportSolution {
 
-  public ExportSolution() {
-    // TODO Auto-generated constructor stub
-  }
+    private static final String FILENAME = "output_e.txt";
 
-  public static void export(List<String> results) {
-    String FILENAME = "output_e.txt";
-    File file = new File(FILENAME);
+    public static void export(List<String> results) {
+        File file = new File(FILENAME);
 
-    try (FileWriter fw = new FileWriter(file.getAbsoluteFile(), true); BufferedWriter bw = new BufferedWriter(fw);) {
+        try (FileWriter fw = new FileWriter(file.getAbsoluteFile(), true); BufferedWriter bw = new BufferedWriter(fw)) {
+            StringBuilder data = new StringBuilder();
+            for (String string : results) {
+                data.append(string + "\n");
+            }
 
-      StringBuilder data = new StringBuilder();
-      for (String string : results) {
-        data.append(string + "\n");
-      }
+            bw.write(data.toString().trim());
 
-      bw.write(data.toString().trim());
-
-    } catch (IOException e) {
-      e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-  }
 }
